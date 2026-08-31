9287ec3d92ad65e5c2eaac93d628468d3c9079d4
gacoan449 <gacoan449@gmail.com>
2026-08-31 04:02:32 -0700

    fix(android): add Flutter v2 MainActivity

---
+++ android/app/src/main/kotlin/Nusopa/mart/MainActivity.kt
@@ -0,0 +1,9 @@
+package Nusopa.mart
+
+import io.flutter.embedding.android.FlutterActivity
+
+/**
+ * Main Android activity using Flutter's current Android embedding (v2).
+ * Do not replace this with the deprecated io.flutter.app.FlutterActivity.
+ */
+class MainActivity : FlutterActivity()
