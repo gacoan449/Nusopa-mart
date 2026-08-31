88b9cc4e3d41624902f4babad248c55d25f4e410
gacoan449 <gacoan449@gmail.com>
2026-08-31 01:45:56 -0700

    Add Firebase configuration placeholder

---
+++ lib/firebase_options.dart
@@ -0,0 +1,6 @@
+// PLACEHOLDER: jalankan 'flutterfire configure' setelah menghubungkan Firebase Console.
+// File ini sengaja harus diganti oleh konfigurasi Firebase asli.
+class DefaultFirebaseOptions {
+  static dynamic get currentPlatform =>
+      throw UnsupportedError('Jalankan flutterfire configure untuk membuat konfigurasi Firebase.');
+}
