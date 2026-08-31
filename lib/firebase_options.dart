cffec76c466a317c957dd663a30db4bea32d093c
gacoan449 <gacoan449@gmail.com>
2026-08-31 01:59:21 -0700

    Configure Android Firebase options from uploaded project config

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,6 +1,17 @@
-// PLACEHOLDER: jalankan 'flutterfire configure' setelah menghubungkan Firebase Console.
-// File ini sengaja harus diganti oleh konfigurasi Firebase asli.
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
+
 class DefaultFirebaseOptions {
-  static dynamic get currentPlatform =>
-      throw UnsupportedError('Jalankan flutterfire configure untuk membuat konfigurasi Firebase.');
+  static FirebaseOptions get currentPlatform {
+    if (defaultTargetPlatform == TargetPlatform.android) return android;
+    throw UnsupportedError('Platform belum dikonfigurasi. Jalankan flutterfire configure untuk iOS/Web.');
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyB-guTai42pYgcvXeacyt_8fyUpkfxzluA',
+    appId: '1:242778852239:android:746f364459e64879fe99fa',
+    messagingSenderId: '242778852239',
+    projectId: 'desapay-10614',
+    storageBucket: 'desapay-10614.firebasestorage.app',
+  );
 }
