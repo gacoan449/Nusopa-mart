9b470a1ef2beecd6fa577fb62f6f45fc20c83c43
gacoan449 <gacoan449@gmail.com>
2026-08-31 01:45:53 -0700

    Initialize Flutter with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'screens/auth_gate.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const NusopaMartApp());
 }
-
 class NusopaMartApp extends StatelessWidget {
   const NusopaMartApp({super.key});
-
   @override
-  Widget build(BuildContext context) {
-    const orange = Color(0xFFFF5722);
-    return MaterialApp(
-      title: 'Nusopa.Mart',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: orange),
-        textTheme: GoogleFonts.interTextTheme(),
-      ),
-      home: const LoginScreen(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Nusopa.Mart',
+    theme: ThemeData(useMaterial3: true, colorSchemeSeed: const Color(0xFFFF5722)),
+    home: const AuthGate(),
+  );
 }
