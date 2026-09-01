03910945164441dc12d46d1aad9a9fe9842763a5
gacoan449 <gacoan449@gmail.com>
2026-09-01 05:46:38 -0700

    feat: initialize global incoming-call FCM and Firestore listener

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'screens/auth_gate.dart';
+import 'services/call_notification_service.dart';
+
+final GlobalKey<NavigatorState> appNavigatorKey = GlobalKey<NavigatorState>();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await CallNotificationService.instance.initialize(appNavigatorKey);
   runApp(const NusopaMartApp());
 }
+
 class NusopaMartApp extends StatelessWidget {
   const NusopaMartApp({super.key});
   @override
   Widget build(BuildContext context) => MaterialApp(
+    navigatorKey: appNavigatorKey,
     debugShowCheckedModeBanner: false,
     title: 'Nusopa.Mart',
     theme: ThemeData(useMaterial3: true, colorSchemeSeed: const Color(0xFFFF5722)),
